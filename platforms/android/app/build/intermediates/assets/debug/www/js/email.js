c181617fad19690207572bca72af796261146c08
Arran Crowley <acrowley@student.scalda.nl>
2019-04-10 20:37:01 +0200

    Initial commit code

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/email.js
@@ -0,0 +1,29 @@
+$(document).on('click', "#emailOpvragen", function () {
+    // variable answer with answer from user
+    var inputtedEmail = $('#emailInput').val();
+    var inputtedNaam = $('#naamInput').val();
+
+    console.log(inputtedNaam);
+    console.log(inputtedEmail);
+    // variable with id from answered question
+    var Opleiding_Id = localStorage.getItem('gekozenOpleiding');
+    console.log(Opleiding_Id);
+    // variable datastring with data to send to server
+    var dataString = "email=" + inputtedEmail + "&naam=" + inputtedNaam + "&opleidingID=" + Opleiding_Id;
+    // ajax function
+    $.ajax({
+        // url to send data to
+        url: "http://arranji330.330.axc.nl/Vragenapp_Versie_2.1/emailpagina/",
+        // type post because it sends data
+        type: 'POST',
+        // allow to send data to a other domain
+        crossDomain: true,
+        // data om te verzenden
+        data: dataString,
+        // on succes of the ajax function fill localStorage with data
+        success: function () {
+            alert("Email toegevoegd");
+            location.reload();
+        }
+    });
+});
